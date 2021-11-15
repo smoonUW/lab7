a6dbac44e19973a7a404289cb4822eb0297c9097
smoonUW <smmoon2@wisc.edu>
2021-11-15 13:05:13 -0600

    Milestone 2 complete

--- app/src/main/java/com/example/lab7/MainActivity.java
+++ app/src/main/java/com/example/lab7/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,19 +35,33 @@ public class MainActivity extends AppCompatActivity {
     public void sendOnChannel1(View v) {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
+
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this,
+                0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_baseline_chat_24)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
                 .build();
 
         notificationManager.notify(1, notification);
     }
 
     public void sendOnChannel2(View v) {
-        Log.i("chansend", "channel 2 is sent?");
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
         Notification notification2 = new NotificationCompat.Builder(this, CHANNEL_2_ID)
@@ -56,7 +73,5 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         notificationManager.notify(2, notification2);
-
-        Log.i("channot", "channel 2 notified?");
     }
 }
